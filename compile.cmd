ab834583d671d4d2d640549836d8154db3f64f89
Kirit Sælensminde <k@kirit.com>
2009-08-24 13:45:57 +0000

    Updates to build scripts.

--- compile.cmd
+++ compile.cmd
@@ -1,12 +1,10 @@
 @echo off
+echo fost-windows
 ..\bjam toolset=msvc %*
 
 IF NOT ERRORLEVEL 1 (
     ..\dist\bin\fost-schema-test-dyndriver -b false ado fost-ado.dll
     IF NOT ERRORLEVEL 1 (
         ..\dist\bin\fost-schema-test-dyndriver -b false ado.f3 fost-ado.dll
-        IF NOT ERRORLEVEL 1 (
-            ..\dist\bin\ftest -b false -i Configuration/ado.ini ..\dist\bin\fost-ado-test-smoke.dll
-        )
     )
 )
