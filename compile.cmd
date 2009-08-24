b452a16834c05b072a7858cf62162e65f00a29f9
Kirit Sælensminde <k@kirit.com>
2009-08-24 04:51:23 +0000

    Restructured compile script.

--- compile.cmd
+++ compile.cmd
@@ -2,17 +2,11 @@
 ..\bjam toolset=msvc %*
 
 IF NOT ERRORLEVEL 1 (
-    call ..\boost-version.cmd
-    for /r %BUILD_DIRECTORY% %%f in (*.pdb) do xcopy /D /Y %%f ..\dist\bin
-)
-
-IF NOT ERRORLEVEL 1 (
     ..\dist\bin\fost-schema-test-dyndriver -b false ado fost-ado.dll
-)
-IF NOT ERRORLEVEL 1 (
-    ..\dist\bin\fost-schema-test-dyndriver -b false ado.f3 fost-ado.dll
-)
-
-IF NOT ERRORLEVEL 1 (
-    ..\dist\bin\ftest -b false -i Configuration/ado.ini ..\dist\bin\fost-ado-test-smoke.dll
+    IF NOT ERRORLEVEL 1 (
+        ..\dist\bin\fost-schema-test-dyndriver -b false ado.f3 fost-ado.dll
+        IF NOT ERRORLEVEL 1 (
+            ..\dist\bin\ftest -b false -i Configuration/ado.ini ..\dist\bin\fost-ado-test-smoke.dll
+        )
+    )
 )
