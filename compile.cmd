739d8b8f0f56b4ae19ee4d037100ff0d07a4a58a
Kirit Sælensminde <k@kirit.com>
2009-09-11 12:00:48 +0000

    The build process has been changed so that the tests are run as part of the build not as part of the compile script.

--- compile.cmd
+++ compile.cmd
@@ -1,10 +1,3 @@
 @echo off
 echo fost-windows
-..\bjam toolset=msvc %*
-
-IF NOT ERRORLEVEL 1 (
-    ..\dist\bin\fost-schema-test-dyndriver -b false ado fost-ado.dll
-    IF NOT ERRORLEVEL 1 (
-        ..\dist\bin\fost-schema-test-dyndriver -b false ado.f3 fost-ado.dll
-    )
-)
+..\bjam toolset=msvc preserve-test-targets=on %*
